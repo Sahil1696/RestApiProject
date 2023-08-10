95a610bf914242285a0b56c70199b09ab39eed8d
Sahil <SharmaSahil2017@gmail.com>
2023-08-11 00:08:17 +0530

    again jenkins testeing

--- RestApiProject/src/main/java/com/project/RestApiProject/Service/CourseDaoImpl.java
+++ RestApiProject/src/main/java/com/project/RestApiProject/Service/CourseDaoImpl.java
@@ -23,7 +23,7 @@ public class CourseDaoImpl implements CourseDao {
     @Override
     public List<Course> getCourse() {
        // return courses;
-        System.out.println("testing jenkins cicd pipeline by doing small comments");
+        System.out.println("testing jenkins cicd pipeline by doing small commentsss");
          return oracleRepository.findAll();
     }
 
